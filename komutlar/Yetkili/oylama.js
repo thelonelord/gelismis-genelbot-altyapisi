3997cb25eeecfeaade2d7cbd8e9131743381e3f8
thelonelord <109240514+thelonelord@users.noreply.github.com>
2022-08-05 14:52:56 +0300

    Update oylama.js

--- komutlar/Yetkili/oylama.js
+++ komutlar/Yetkili/oylama.js
@@ -6,7 +6,7 @@ exports.run = (client, message, args) => {
 const db = require("quick.db")
   let prefix = db.fetch(`prefix_${message.guild.id}`) || ayarlar.prefix;
     
-    const laura = (abcdef) => {
+    const aura = (abc) => {
         message.channel.send(
       new Discord.MessageEmbed().setColor(ayarlar.renk).setDescription("Bir hata oluştu.")
     );
@@ -14,26 +14,26 @@ const db = require("quick.db")
 
    
     try {
-        const rifleman = args.join(' ').split('/')
-        const youthanasia = rifleman[0].trim();
-        if (!youthanasia) return laura('Komudu yanlış kullanmış olabilirsiniz, doğru kullanımı: \n\n`${prefix}poll [başlık] / (şık1), (şık2), (şık3)`\n* ***Başlıktan sonra / koyun ve şıkları virgül ile ayırın.***');
-        const lauraa = rifleman[1].trim().split(',');
-        const lauraaa = lauraa.length;
-        if (lauraaa > 10) return laura('Maksimum 10 seçenek koyabilirsiniz.');
-        if (lauraa.includes(' ')) return laura('Boş bir şık koyamazsınız.')
+        const rif = args.join(' ').split('/')
+        const you = rif[0].trim();
+        if (!you) return aura('Komudu yanlış kullanmış olabilirsiniz, doğru kullanımı: \n\n`${prefix}oylama [başlık] / (şık1), (şık2), (şık3)`\n* ***Başlıktan sonra / koyun ve şıkları virgül ile ayırın.***');
+        const auraa = rif[1].trim().split(',');
+        const auraaa = auraa.length;
+        if (auraaa > 10) return aura('Maksimum 10 seçenek koyabilirsiniz.');
+        if (auraa.includes(' ')) return aura('Boş bir şık koyamazsınız.')
         
         const pollEmbed = new Discord.MessageEmbed().setTitle('Lütfen bekleyiniz ayarlamalar yapılıyor..');
         const emojies = ['1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣', '6️⃣', '7️⃣', '8️⃣', '9️⃣', '🔟'];
         message.channel.send(pollEmbed).then(async pollMsg => {
-            for (let lauraaaa = 0; lauraaaa < lauraaa; lauraaaa++) {
-                pollMsg.react(emojies[lauraaaa]);
-                pollEmbed.addField(`${emojies[lauraaaa]} ${lauraa[lauraaaa].trim()}`, `\u200B`, true);
+            for (let auraaaa = 0; auraaaa < auraaa; auraaaa++) {
+                pollMsg.react(emojies[auraaaa]);
+                pollEmbed.addField(`${emojies[auraaaa]} ${auraa[auraaaa].trim()}`, `\u200B`, true);
             };
             
-            pollMsg.edit(pollEmbed.setTitle(youthanasia).setFooter('Seçeneğin emojisine tıklayarak oylamaya katılabilirsiniz.'));
+            pollMsg.edit(pollEmbed.setTitle(you).setFooter('Seçeneğin emojisine tıklayarak oylamaya katılabilirsiniz.'));
         });
     } catch(err) {
-        laura('Komudu yanlış kullanmış olabilirsiniz, doğru kullanımı: \n\n`${prefix}oylama [başlık] / (şık1), (şık2), (şık3)`\n* ***Başlıktan sonra / koyun ve şıkları virgül ile ayırın.***');
+        aura('Komudu yanlış kullanmış olabilirsiniz, doğru kullanımı: \n\n`${prefix}oylama [başlık] / (şık1), (şık2), (şık3)`\n* ***Başlıktan sonra / koyun ve şıkları virgül ile ayırın.***');
     }
 };
 
